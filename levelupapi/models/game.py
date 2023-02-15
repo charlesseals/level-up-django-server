9f0908cd7f7f7147696903e1bfb590e5bc5bc5af
Charles Seals <charlesseals11@gmail.com>
2023-02-15 11:45:47 -0600

    first commit with a bunch of new files added

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -6,3 +6,5 @@ class Game(models.Model):
     description = models.CharField(max_length=200)
     game_type = models.ForeignKey("GameType", on_delete=models.CASCADE,related_name='games')
     designer = models.CharField(max_length=40)
+    number_of_players = models.IntegerField(default=1)
+    skill_level = models.CharField(default="medium", max_length=15)
